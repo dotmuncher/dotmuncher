a9570b626d77cd6e1ee41c4afb1e2f7041244c23
Yi Wang <e@yi-wang.me>
2010-11-10 21:25:17 -0500

    put handlers in DMAppControler

--- android/src/com/dotmuncher/android/controler/DMAppControler.java
+++ android/src/com/dotmuncher/android/controler/DMAppControler.java
@@ -12,4 +12,27 @@ public class DMAppControler {
 		phoneToken = "a_" + deviceId;
 	}	
 
+	public void update_phone_settings(){
+		
+	}
+	
+	public void find_games(){
+		
+	}
+	
+	public void find_maps(){
+		
+	}
+	
+	public void new_game(){
+		
+	}
+	
+	public void join_game(){
+		
+	}
+	
+	public void update(){
+		
+	}
 }
--- android/src/com/dotmuncher/android/events/DMEvents.java
+++ android/src/com/dotmuncher/android/events/DMEvents.java
@@ -4,6 +4,7 @@ import com.google.android.maps.GeoPoint;
 import java.util.List;
 
 public class DMEvents {
-
+	public static int GAMEOVER_PACMAN_WINS = 1;
+	public static int GAMEOVER_PACMAN_LOSES = 2;
 
 }
