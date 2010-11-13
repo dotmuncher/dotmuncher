a71e5c3491be039163feefcb3be2d1adf5bef842
Andrew Schaaf <andrew@andrewschaaf.com>
2010-11-13 17:27:19 -0500

    tests/all now adds dotmuncher/.. to sys.path

--- tests/all.py
+++ tests/all.py
@@ -1,4 +1,13 @@
 
+# Add ROOT to sys.path
+def parentOf(path, n=1):
+    return '/'.join(path.rstrip('/').split('/')[:-n])
+ROOT = parentOf(os.path.abspath(__file__), n=3)
+import sys
+sys.path.append(ROOT)
+
+
+
 import unittest
 
 from dotmuncher.tests.system import *
