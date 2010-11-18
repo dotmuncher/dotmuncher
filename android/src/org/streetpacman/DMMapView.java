390d5e7597a89578e63dcd2f5e2273558e5daf0a
Yi Wang <e@yi-wang.me>
2010-11-18 00:28:29 -0500

    overLay image on map

--- android/src/org/streetpacman/DMMapView.java
+++ android/src/org/streetpacman/DMMapView.java
@@ -16,20 +16,84 @@
 
 package org.streetpacman;
 
+import java.util.List;
+
 import org.streetpacman.R;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
 import android.os.Bundle;
 
 public class DMMapView extends MapActivity {
+    MapView mapView; 
+    MapController mc;
+    GeoPoint p;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mapview);
+        
+        mapView = new MapView(this, "MapViewCompassDemo_DummyAPIKey");
+        //setContentView(R.layout.mapview);
+        setContentView(mapView);
+        
+
+        
+        String coordinates[] = {"1.352566007", "103.78921587"};
+        double lat = Double.parseDouble(coordinates[0]);
+        double lng = Double.parseDouble(coordinates[1]);
+ 
+        p = new GeoPoint(
+            (int) (lat * 1E6), 
+            (int) (lng * 1E6));
+
+        //*/...
+        mc = mapView.getController();
+
+        mc.animateTo(p);
+        mc.setZoom(17); 
+ 
+        //---Add a location marker---
+        MapOverlay mapOverlay = new MapOverlay();
+        List<Overlay> listOfOverlays = mapView.getOverlays();
+        listOfOverlays.clear();
+        listOfOverlays.add(mapOverlay);        
+ 
+        mapView.invalidate();
+        //*/
     }
 
     @Override
     protected boolean isRouteDisplayed() { return false; }
+    
+    class MapOverlay extends com.google.android.maps.Overlay
+    {
+    	
+        @Override
+        public boolean draw(Canvas canvas, MapView mapView, 
+        boolean shadow, long when) 
+        {
+            super.draw(canvas, mapView, shadow);                   
+ 
+            //---translate the GeoPoint to screen pixels---
+            Point screenPts = new Point();
+            
+            mapView.getProjection().toPixels(p, screenPts);
+ 
+            //---add the marker---
+            Bitmap bmp = BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_chomp0);            
+            canvas.drawBitmap(bmp, screenPts.x, screenPts.y-50, null);         
+            return true;
+        }
+    } 
+
 }
