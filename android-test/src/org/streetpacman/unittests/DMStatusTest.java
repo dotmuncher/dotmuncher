81e38f2dead29aae7c215783f93344db1125b436
Yi Wang <e@yi-wang.me>
2010-12-19 19:56:00 -0500

    multi set status

--- android-test/src/org/streetpacman/unittests/DMStatusTest.java
+++ android-test/src/org/streetpacman/unittests/DMStatusTest.java
@@ -11,7 +11,7 @@ public class DMStatusTest extends TestCase {
 		
 		s.set(IStatus.VISIBLE_HIDE_GLOBAL);
 		assertTrue(s.is(IStatus.VISIBLE_HIDE_GLOBAL));
-		
+
 		s.unset(IStatus.VISIBLE_HIDE_GLOBAL);
 		assertFalse(s.is(IStatus.VISIBLE_HIDE_GLOBAL));
 		
@@ -20,5 +20,18 @@ public class DMStatusTest extends TestCase {
 		
 		s.setPhase(IStatus.PHASE_PLAYING);
 		assertTrue(s.isPhase(IStatus.PHASE_PLAYING));
+		
+		// multi set
+		s.set(IStatus.VISIBLE_TO_OBSERVER | IStatus.PLAYER_ALIVE | IStatus.PLAYER_FROZEN);
+		assertTrue(s.is(IStatus.VISIBLE_TO_OBSERVER));
+		assertTrue(s.is(IStatus.PLAYER_ALIVE));
+		assertTrue(s.is(IStatus.PLAYER_FROZEN));
+		assertTrue(s.is(IStatus.VISIBLE_TO_OBSERVER | IStatus.PLAYER_ALIVE | IStatus.PLAYER_FROZEN));
+
+		s.unset(IStatus.VISIBLE_TO_OBSERVER | IStatus.PLAYER_ALIVE | IStatus.PLAYER_FROZEN);
+		assertFalse(s.is(IStatus.VISIBLE_TO_OBSERVER));
+		assertFalse(s.is(IStatus.PLAYER_ALIVE));
+		assertFalse(s.is(IStatus.PLAYER_FROZEN));
+		assertFalse(s.is(IStatus.VISIBLE_TO_OBSERVER | IStatus.PLAYER_ALIVE | IStatus.PLAYER_FROZEN));
 	}
 }
